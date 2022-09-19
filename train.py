11cc7b9aa4f93765f27adfa6617e48b10050aca4
zhufengning <renhaihao@outlook.com>
2022-09-19 16:27:32 +0800

    Update train.py

--- train.py
+++ train.py
@@ -107,7 +107,9 @@ def run(rank, n_gpus, hps):
         _, _, _, epoch_str = utils.load_checkpoint(
             utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d, optim_d)
         global_step = (epoch_str - 1) * len(train_loader)
+        print("loaded checkpoint")
     except:
+        print("no checkpoint")
         epoch_str = 1
         global_step = 0
 
